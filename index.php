03a55d7c5ad8f9fd1ee1f314b91d5dfaf469cf01
massa <massafranck@gmail.com>
2018-07-29 23:12:38 +0200

    indentation

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
- <!DOCTYPE html>
- <html lang="fr">
-   <head>
-     <meta charset="utf-8" />
-     <title>Exercice1</title>
-   </head>
-   <body>
-     <p>
-<?php
-//La fonction true retourne la valeur true
-function true() {
-            return true;
-        }
-//on affiche la valeur true
-        echo true();
-?>
-     </p>
-   </body>
- </html>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice1</title>
+</head>
+<body>
+  <p>
+    <?php
+    //La fonction true retourne la valeur true
+    function true() {
+      return true;
+    }
+    //on affiche la valeur true
+    echo true();
+    ?>
+  </p>
+</body>
+</html>
